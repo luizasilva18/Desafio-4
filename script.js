f0ecc956b7ceef5b4caf5698e2059698c659fad5
luizasilva18clear <luizagoncalves7@gmail.com>
2021-05-27 17:59:58 -0300

    Desafio de funções

---
+++ script.js
@@ -0,0 +1,32 @@
+function apresentação(){
+    console.log ("Olá, bem vindos!")
+
+}
+apresentação()
+
+function mostrarnome (nome){
+    console.log ("Meu nome é", nome )
+}
+mostrarnome ("Luíza")
+
+
+function soma (x,y,z){
+ return x + y + z
+}
+soma ("Luiza", 20 , "R&B")
+console.log(soma("Luiza" , 20 , "R&B"))
+
+
+function soma2 (x,y){
+  return x + y 
+ }
+ soma2 ("Soul","Várias queixas")
+ console.log(soma("Soul", "Várias queixas"))
+ 
+
+function triplo (num){
+  return num*3
+}
+triplo (3);
+console.log(triplo(3))
+
